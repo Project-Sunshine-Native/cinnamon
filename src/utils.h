616f514e6e52a8f4115a78d5186a662d258d854e
MrPowerGamerBR <git@mrpowergamerbr.com>
2026-03-02 22:03:40 -0300

    Add require and requireMessage macros
    Similar to Kotlin's require function

--- src/utils.h
+++ src/utils.h
@@ -5,3 +5,19 @@
 #define forEachIndexed(item, index, array, count) for (typeof(count) idx = 0; idx < (count) && ((item) = &(array)[idx], 1); idx++)
 
 #define repeat(n, it) for (typeof(n) it = 0; it < (n); it++)
+
+#define require(condition) \
+    do { \
+        if (!(condition)) { \
+        fprintf(stderr, "Requirement failed at %s:%d\n", __FILE__, __LINE__); \
+        abort(); \
+    } \
+} while (0)
+
+#define requireMessage(condition, message) \
+do { \
+if (!(condition)) { \
+fprintf(stderr, "Requirement failed at %s:%d: %s\n", __FILE__, __LINE__, message); \
+abort(); \
+} \
+} while (0)
