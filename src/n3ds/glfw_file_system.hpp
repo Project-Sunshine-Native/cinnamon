750109b9836e7e9bda3f9396dfe6ada5e6712cdb
casriel. <adlerriggstablet@gmail.com>
2026-04-20 18:24:54 -0700

    Butterscotch SRC update, various improvements

--- src/n3ds/glfw_file_system.hpp
+++ src/n3ds/glfw_file_system.hpp
@@ -5,9 +5,9 @@
 typedef struct {
     FileSystem base;
     char* basePath; // directory containing data.win, with trailing separator
-} N3DSFileSystem;
+} GlfwFileSystem;
 
-// Creates a N3DSFileSystem from the path to the data.win file
+// Creates a GlfwFileSystem from the path to the data.win file
 // The basePath is derived by stripping the filename from dataWinPath.
-N3DSFileSystem* N3DSFileSystem_create(const char* dataWinPath);
-void N3DSFileSystem_destroy(N3DSFileSystem* fs);
+GlfwFileSystem* GlfwFileSystem_create(const char* dataWinPath);
+void GlfwFileSystem_destroy(GlfwFileSystem* fs);
